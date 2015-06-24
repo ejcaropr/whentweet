7f7fbd0a649cf6ccd1c5b0b239e6dd692d5dd4aa
Eduardo Caro <ejcaropr@gmail.com>
2015-06-24 00:14:37 -0700

    Make into a class

--- whentweet.py
+++ whentweet.py
@@ -4,7 +4,10 @@ import twitter_oauth
 import tweepy
 import pandas as pd
 from tweepy import API
-
+from datetime import timedelta as td
+from datetime import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
 
 auth = tweepy.OAuthHandler(twitter_oauth.CONSUMER_KEY, twitter_oauth.CONSUMER_SECRET)
 auth.set_access_token(twitter_oauth.ACCESS_TOKEN, twitter_oauth.ACCESS_SECRET)
@@ -12,6 +15,23 @@ auth.set_access_token(twitter_oauth.ACCESS_TOKEN, twitter_oauth.ACCESS_SECRET)
 myapi = API(auth)
 
 def getTweets(user):
-	return tweepy.Cursor(myapi.user_timeline, id = user).items()
-	
+	return list(tweepy.Cursor(myapi.user_timeline, id = user).items())
+
+class Wt:
+	def __init__(self,user,limit=1000):
+		self.cursor = tweepy.Cursor(myapi.user_timeline, id = user).items(limit)
+		self.user = myapi.get_user(user)
+		self.tweets = list(self.cursor)
+		if self.user.utc_offset == None:
+			self.offset = td()
+		else:
+			self.offset = td(seconds=self.user.utc_offset)
+
+	def plot(self):
+		t = [(t.created_at + self.offset).time() for t in self.tweets]
+		d = [(t.created_at + self.offset).date() for t in self.tweets]
+		colors = [ (t.created_at + self.offset).weekday() > 4 for t in self.tweets]
+
+		plt.scatter(d,t,c = colors, alpha=0.5, s=100)
+		plt.show()
 
